523619f1593603e2da1bd17c0de960daca53cbb0
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2024-01-23 15:50:53 -0800

    fix: do not regenerate external ID

--- github_publisher.py
+++ github_publisher.py
@@ -458,8 +458,9 @@ class GitHubPublisher(guru.PublisherFolders):
         response = self.get_repository_content(expected_path)
 
         if response.ok:
-            external_id = self.generate_external_id(collection.id, response.json())
-
+            external_id = self.get_metadata(collection.id)[
+                "external_id"
+            ] or self.generate_external_id(collection.id, response.json())
             return external_id
 
     def create_external_collection(self, collection: guru.Collection):
@@ -540,7 +541,9 @@ class GitHubPublisher(guru.PublisherFolders):
         response = self.get_repository_content(expected_path)
 
         if response.ok:
-            external_id = self.generate_external_id(folder.id, response.json())
+            external_id = self.get_metadata(folder.id)[
+                "external_id"
+            ] or self.generate_external_id(folder.id, response.json())
             return external_id
 
     def create_external_folder(self, folder: guru.Folder, collection: guru.Collection):
@@ -626,7 +629,9 @@ class GitHubPublisher(guru.PublisherFolders):
         response = self.get_repository_content(expected_path)
 
         if response.ok:
-            external_id = self.generate_external_id(card.id, response.json())
+            external_id = self.get_metadata(card.id)[
+                "external_id"
+            ] or self.generate_external_id(card.id, response.json())
             return external_id
 
     def convert_card_content(self, card: guru.Card):
